2de537869219a08596faec2bda166cadcb00e9c7
moeabdol <mohd.a.saed@gmail.com>
2017-12-10 09:04:14 +0300

    Implement signin with google

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent {
+  constructor(private _afAuth: AngularFireAuth) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  signin() {
+    this._afAuth.auth
+      .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
-
 }
